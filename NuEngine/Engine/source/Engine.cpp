ab6a4fef9691bb763a93e6becb9c1a43f7b5236f
Marcus Reid <NuMakesGames@users.noreply.github.com>
2024-08-15 05:33:00 +0100

    Setup basic project structure.

---
+++ NuEngine/Engine/source/Engine.cpp
@@ -0,0 +1,11 @@
+// Engine.cpp : Defines the functions for the static library.
+//
+
+#include <string>
+#include <NuEngine/Engine.h>
+
+// TODO: This is an example of a library function
+std::string fnEngine()
+{
+	return "Hello from Engine!";
+}
---
+++ NuEngine/Engine/source/include/NuEngine/Engine.h
@@ -0,0 +1,3 @@
+#pragma once
+
+class std::string fnEngine();
